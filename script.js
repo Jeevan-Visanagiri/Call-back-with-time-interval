bdaab2a30b1a55e3cadc4208366e07ebd0d431f6
Jeevan Visanagiri <Jeevan-Visanagiri@users.noreply.github.com>
2020-07-22 22:29:09 +0530

    NEXT commit

--- script.js
+++ script.js
@@ -1,9 +1,52 @@
+var h1 = document.getElementById('header')
+function countdown() {
+    setTimeout(() => {
+        h1.innerHTML = '10..',
+            setTimeout(() => {
+                h1.innerHTML = '9..',
+                    setTimeout(() => {
+                        h1.innerHTML = '8..',
+                            setTimeout(() => {
+                                h1.innerHTML = '7..',
+                                    setTimeout(() => {
+                                        h1.innerHTML = '6..',
+                                            setTimeout(() => {
+                                                h1.innerHTML = '5..',
+                                                    setTimeout(() => {
+                                                        h1.innerHTML = '4..',
+                                                            setTimeout(() => {
+                                                                h1.innerHTML = '3..',
+                                                                    setTimeout(() => {
+                                                                        h1.innerHTML = '2..',
+                                                                            setTimeout(() => {
+                                                                                h1.innerHTML = '1..',
+                                                                                    setTimeout(() => {
+                                                                                        h1.innerHTML = 'HAPPY MORNING'
+                                                                                    }, 1000)
+                                                                            }, 1000)
+                                                                    }, 1000)
+                                                            }, 1000)
+                                                    }, 1000)
+                                            }, 1000)
+                                    }, 1000)
+                            }, 1000)
+                    }, 1000)
+            }, 1000)
+    }, 1000)
+}
 
 
 
+countdown();
+
+
+
+
+
+/*
 setInterval(function(){
     if(name==0)
-      clearInterval();
+        clearInterval();
     else
     {
     console.log((name--)+"..");
@@ -24,13 +67,13 @@ setInterval(function(){
         }
         else
         console.log(name--+"..");
-    
+
     },1000);
   }
-  
+
   function processUserInput(callback) {
     callback(10);
   }
-  
+
   processUserInput(greeting);
   */
